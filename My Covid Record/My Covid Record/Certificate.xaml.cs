f5476a28b0533e29c90e7dfaf2e015b6e14529a1
smaxerene <smquilat@gmail.com>
2023-06-12 14:12:43 +1200

    QR, Details, & Certificate

---
+++ My Covid Record/My Covid Record/Certificate.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace My_Covid_Record
+{
+    /// <summary>
+    /// Interaction logic for Certificate.xaml
+    /// </summary>
+    public partial class Certificate : Window
+    {
+        public Certificate()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Homepage homepage = new Homepage();
+            homepage.Show();
+            Close();
+        }
+
+        //Account 
+        private void btnClosePopup_Click(object sender, RoutedEventArgs e)
+        {
+            myPopup.IsOpen = false;
+        }
+
+        private void btnShowPopup_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == btnShowPopup)
+            {
+                myPopup.IsOpen = true;
+            }
+        }
+
+        private void Edit_Click(object sender, RoutedEventArgs e)
+        {
+            VaccineDetailsPage details = new VaccineDetailsPage();
+            details.Show();
+            Close();
+        }
+        private void Download_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+    }
+}
---
+++ My Covid Record/My Covid Record/VaccineDetailsPage.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace My_Covid_Record
+{
+    /// <summary>
+    /// Interaction logic for VaccineDetailsPage.xaml
+    /// </summary>
+    public partial class VaccineDetailsPage : Window
+    {
+        public VaccineDetailsPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Home_Click(object sender, RoutedEventArgs e)
+        {
+            Homepage homepage = new Homepage();
+            homepage.Show();
+            Close();
+        }
+
+        //Account 
+        private void btnClosePopup_Click(object sender, RoutedEventArgs e)
+        {
+            myPopup.IsOpen = false;
+        }
+
+        private void btnShowPopup_Click(object sender, RoutedEventArgs e)
+        {
+            if (sender == btnShowPopup)
+            {
+                myPopup.IsOpen = true;
+            }
+        }
+
+        private void EditUpload_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void GenerateCert_Click(object sender, RoutedEventArgs e)
+        {
+            Certificate certificate = new Certificate();
+            certificate.Show();
+            Close();
+        }
+    }
+}
